0c16c6951577e344c3103f739f84ee6341efae38
anamariagr <anamariagarcia9709@gmail.com>
2024-08-05 11:08:28 -0500

    feat: delete and edit in table

--- lib/apps/samiya/views/samiya_table.dart/samiya_tara.dart
+++ lib/apps/samiya/views/samiya_table.dart/samiya_tara.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sens2/core/components/modals/menu_drawer.dart'; 
+import 'package:sens2/core/components/modals/menu_drawer.dart';
 
 class SamiyaTara extends StatelessWidget {
   final List<Map<String, dynamic>> data = [
@@ -39,13 +39,12 @@ class SamiyaTara extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.arrow_back),
             onPressed: () {
-              Get.back(); 
+              Get.back();
             },
           ),
         ],
       ),
-       drawer: MenuDrawer(),
-
+      drawer: MenuDrawer(),
       body: Stack(
         children: [
           Column(
@@ -63,11 +62,10 @@ class SamiyaTara extends StatelessWidget {
                         ),
                       ),
                     ),
-                   Expanded(
+                    Expanded(
                       child: Container(
-                        width: double.infinity, 
+                        width: double.infinity,
                         height: 50.0,
-              
                         child: TextField(
                           decoration: InputDecoration(
                             hintText: 'Filtrar',
@@ -75,47 +73,38 @@ class SamiyaTara extends StatelessWidget {
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(8.0),
                             ),
-                              contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0), 
-     
+                            contentPadding:
+                                EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
                           ),
                         ),
                       ),
                     ),
                     Container(
-                      width: 50.0, 
-                      height: 50.0, 
+                      width: 50.0,
+                      height: 50.0,
                       padding: EdgeInsets.all(8.0),
                       child: IconButton(
                         icon: Icon(Icons.filter_list),
-                        onPressed: () {
-                        
-                        },
+                        onPressed: () {},
                       ),
                     ),
-
-                   
                   ],
                 ),
               ),
-              Positioned(
-                top: 96.0, 
-                left: 0,
-                right: 0,
-                child: Container(
-                  color: Color.fromARGB(22, 168, 161, 161), 
-                  child: RawScrollbar(
+              Container(
+                color: Color.fromARGB(22, 168, 161, 161),
+                child: RawScrollbar(
+                  controller: _horizontalScrollController,
+                  thumbVisibility: true,
+                  thickness: 8,
+                  radius: Radius.circular(10),
+                  thumbColor: Color.fromARGB(255, 25, 38, 83),
+                  child: SingleChildScrollView(
                     controller: _horizontalScrollController,
-                    thumbVisibility: true,
-                    thickness: 8,
-                    radius: Radius.circular(10),
-                    thumbColor: Color.fromARGB(255, 25, 38, 83), 
-                    child: SingleChildScrollView(
-                      controller: _horizontalScrollController,
-                      scrollDirection: Axis.horizontal,
-                      child: Container(
-                        width: 1000, 
-                        height: 20, 
-                      ),
+                    scrollDirection: Axis.horizontal,
+                    child: Container(
+                      width: 1000,
+                      height: 20,
                     ),
                   ),
                 ),
@@ -135,16 +124,48 @@ class SamiyaTara extends StatelessWidget {
                         DataColumn(label: Text('OPCIONES')),
                       ],
                       rows: data.map((row) {
+                        final GlobalKey iconKey = GlobalKey();
                         return DataRow(cells: [
                           DataCell(Text(row['fecha_creacion'].toString())),
                           DataCell(Text(row['TARA'])),
                           DataCell(Text(row['PESO'].toString())),
-                          DataCell(IconButton(
-                            icon: Icon(Icons.more_vert),
-                            onPressed: () {
-                           
-                            },
-                          )),
+                          DataCell(
+  Builder(
+    builder: (BuildContext context) {
+      return IconButton(
+        key: iconKey,
+        icon: Icon(Icons.more_vert),
+        onPressed: () {
+          final RenderBox renderBox = iconKey.currentContext?.findRenderObject() as RenderBox;
+          final Offset offset = renderBox.localToGlobal(Offset.zero);
+          showMenu(
+            context: context,
+            position: RelativeRect.fromLTRB(
+              offset.dx, offset.dy, offset.dx + 1, offset.dy + 1,
+            ),
+            items: [
+              PopupMenuItem<String>(
+                value: 'edit',
+                child: Text('Editar'),
+              ),
+              PopupMenuItem<String>(
+                value: 'delete',
+                child: Text('Borrar'),
+              ),
+            ],
+          ).then((value) {
+            if (value == 'edit') {
+              // Lógica para editar
+            } else if (value == 'delete') {
+              // Lógica para borrar
+            }
+          });
+        },
+      );
+    },
+  ),
+),
+
                         ]);
                       }).toList(),
                     ),
@@ -156,103 +177,111 @@ class SamiyaTara extends StatelessWidget {
         ],
       ),
       floatingActionButton: ClipRRect(
-  borderRadius: BorderRadius.circular(50.0), 
-  child: FloatingActionButton(
-    backgroundColor: Color.fromARGB(255, 25, 38, 83),
-    child: Icon(Icons.add),
-    onPressed: () {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            backgroundColor: Color.fromARGB(255, 255, 253, 244),
-            titlePadding: EdgeInsets.zero,
-            title: Container(
-              padding: EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 25, 38, 83),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15.0), // Radio de la esquina superior izquierda
-                  topRight: Radius.circular(15.0), // Radio de la esquina superior derecha
-                ),
-              ),
-              child: Text(
-                'Agregar',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Tara',
+        borderRadius: BorderRadius.circular(50.0),
+        child: FloatingActionButton(
+          backgroundColor: Color.fromARGB(255, 25, 38, 83),
+          child: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Peso',
-                  ),
-                ),
-              ],
-            ),
-            actions: [
-              Container(
-                width: 100, // Ajusta el ancho según sea necesario
-                child: TextButton(
-                  child: Text(
-                    'Cancelar',
-                    style: TextStyle(color: Color.fromARGB(255, 21, 19, 107)), // Color del texto
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 231, 230, 230)), // Color de fondo
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0), // Borde redondeado
-                        side: BorderSide(color: Color.fromARGB(255, 201, 201, 201), width: 2.0), // Borde con color y ancho
+                  backgroundColor: Color.fromARGB(255, 255, 253, 244),
+                  titlePadding: EdgeInsets.zero,
+                  title: Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 25, 38, 83),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15.0),
+                        topRight: Radius.circular(15.0),
                       ),
                     ),
-                  ),
-                ),
-              ),
-              Container(
-                width: 100, // Ajusta el ancho según sea necesario
-                child: TextButton(
-                  child: Text(
-                    'Agregar',
-                    style: TextStyle(color: Colors.white), // Color del texto
-                  ),
-                  onPressed: () {
-                    // Implement save functionality here
-                    Navigator.of(context).pop();
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 22, 37, 92)), // Color de fondo
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0), // Borde redondeado
-                        side: BorderSide(color: Color.fromARGB(255, 21, 14, 90), width: 2.0), // Borde con color y ancho
-                      ),
+                    child: Text(
+                      'Agregar',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
                   ),
-                ),
-              ),
-            ],
-          );
-        },
-      );
-    },
-  ),
-),
-
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Tara',
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Peso',
+                        ),
+                      ),
+                    ],
+                  ),
+                  actions: [
+                    Container(
+                      width: 100,
+                      child: TextButton(
+                        child: Text(
+                          'Cancelar',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 21, 19, 107)),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(
+                                  Color.fromARGB(255, 231, 230, 230)),
+                          shape: MaterialStateProperty.all<
+                              RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              side: BorderSide(
+                                  color: Color.fromARGB(255, 201, 201, 201),
+                                  width: 2.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      child: TextButton(
+                        child: Text(
+                          'Agregar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          // Implementar funcionalidad de guardar aquí
+                          Navigator.of(context).pop();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromARGB(255, 22, 37, 92)),
+                          shape: MaterialStateProperty.all<
+                              RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              side: BorderSide(
+                                  color: Color.fromARGB(255, 21, 14, 90),
+                                  width: 2.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
-
